671c2c71cccf267d4404d439a7a4ccb9becc78f2
YUE Daian <sheepduke@gmail.com>
2019-07-25 17:25:26 +0800

    feat: add `--profile` option

--- backend/src/main.lisp
+++ backend/src/main.lisp
@@ -5,17 +5,26 @@
     (:name :help
      :description "print this help text"
      :short #\h
-     :long "help"))
+     :long "help")
+    (:name :profile
+     :description "set profile to use"
+     :short #\p
+     :long "profile"
+     :arg-parser (lambda (x) (make-keyword (string-upcase x)))))
   (let* ((options (unix-opts:get-opts args))
          (help (getf options :help))
-         (profile (getf options :profile)))
+         (profile (if-let (custom-profile (getf options :profile))
+                    custom-profile
+                    :product)))
     (when help
       (print-help-and-quit))
-    (set-profile :product)
+    (set-profile profile)
     (uiop:chdir (get-config :app-root))
     (setf *default-pathname-defaults* (uiop:getcwd))
     (setup-db)
     (start-server)))
+
+(set-profile :develop)
 
 (defun panic (control-string &rest format-arguments)
   (apply #'format
@@ -27,7 +36,7 @@
 (defun print-help-and-quit ()
   (opts:describe :prefix "Silver Brain - Your external memory device."
                  :suffix "
-This is 
+This is the long description.
 "
                  :usage-of "silver-brain")
   (uiop:quit 0))
