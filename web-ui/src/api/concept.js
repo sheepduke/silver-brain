6149f0b8d00f29f7d4a6c1764a31ddf89bac6ca0
YUE Daian <sheepduke@gmail.com>
2019-11-02 17:00:29 +0800

    fix: add missing `/api` prefix to JS code

--- web-ui/src/api/concept.js
+++ web-ui/src/api/concept.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
-export async function getAllConcepts () {
-  let response = await axios.get('/concepts')
+export async function getAllConcepts() {
+  let response = await axios.get(url(''))
   return response.data
 }
 
@@ -10,59 +10,63 @@ export async function getAllConcepts () {
  *
  * Return: UUID of newly created concept.
  */
-export async function newConcept (name, content, contentFormat) {
+export async function newConcept(name, content, contentFormat) {
   let data = {
     name: name,
     content: content,
     contentFormat: contentFormat
   }
-  let response = await axios.post('/concepts', data)
+  let response = await axios.post(url(''), data)
   if (response.status !== 201) {
     throw Error('Failed to add new concept')
   }
   return response.headers.location.substr(10)
 }
 
-export async function getConceptByUuid (uuid) {
-  let response = await axios.get(`/concepts/${uuid}`)
+export async function getConceptByUuid(uuid) {
+  let response = await axios.get(url(`/${uuid}`))
   return response.data
 }
 
-export async function searchConcept (search) {
-  let response = await axios.get(`/concepts?search=${search}`)
+export async function searchConcept(search) {
+  let response = await axios.get(url(`?search=${search}`))
   return response.data
 }
 
-export async function updateConcept (concept) {
-  await axios.put(`/concepts/${concept.uuid}`, concept)
+export async function updateConcept(concept) {
+  await axios.put(url(`/${concept.uuid}`, concept))
 }
 
-export async function deleteConcept (uuid) {
-  await axios.delete(`/concepts/${uuid}`)
+export async function deleteConcept(uuid) {
+  await axios.delete(url(`/${uuid}`))
 }
 
 /**
  * Return Array<Object{uuid, name}>
  */
-export async function conceptParents (uuid) {
-  let response = await axios.get(`/concepts/${uuid}/parents`)
+export async function conceptParents(uuid) {
+  let response = await axios.get(url(`/${uuid}/parents`))
   return response.data
 }
 
-export async function conceptChildren (uuid) {
-  let response = await axios.get(`/concepts/${uuid}/children`)
+export async function conceptChildren(uuid) {
+  let response = await axios.get(url(`/${uuid}/children`))
   return response.data
 }
 
-export async function conceptFriends (uuid) {
-  let response = await axios.get(`/concepts/${uuid}/friends`)
+export async function conceptFriends(uuid) {
+  let response = await axios.get(url(`/${uuid}/friends`))
   return response.data
 }
 
-export async function removeRelation (relation, uuid, targetUuid) {
-  await axios.delete(`/concepts/${uuid}/${relation}/${targetUuid}`)
+export async function removeRelation(relation, uuid, targetUuid) {
+  await axios.delete(url(`/${uuid}/${relation}/${targetUuid}`))
 }
 
-export async function addRelation (relation, uuid, targetUuid) {
-  await axios.put(`/concepts/${uuid}/${relation}/${targetUuid}`)
+export async function addRelation(relation, uuid, targetUuid) {
+  await axios.put(url(`/${uuid}/${relation}/${targetUuid}`))
+}
+
+function url(part) {
+  return `/api/concepts${part}`
 }
