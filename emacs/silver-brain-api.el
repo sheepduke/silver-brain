f0a2b0c95b4b0de78852fd010cf74602cf3b971a
YUE Daian <sheepduke@gmail.com>
2020-02-14 00:32:57 +0800

    feat: use built-in HTTP facility instead of request library
    The request.el library behaves strangely on Windows.
    
    The built-in `url-retrieve-synchronously` works pretty well.
    And it is BUILT-IN.

--- emacs/silver-brain-api.el
+++ emacs/silver-brain-api.el
@@ -6,8 +6,6 @@
 
 ;;; Code:
 
-(require 'request)
-
 (defvar silver-brain-server-port 5000
   "The port of Silver Brain server.")
 
@@ -36,7 +34,7 @@ Returns the newly created concept."
   "Search concept by KEYWORD.
 Returns a list of concepts."
   (mapcar #'silver-brain-api--plist-to-concept
-          (silver-brain-api--get "/concepts" `(("search" . ,keyword)))))
+          (silver-brain-api--get (format "/concepts?search=%s" keyword))))
 
 (defun silver-brain-api--update-concept (concept)
   "Save CONCEPT to the server."
@@ -84,38 +82,63 @@ Returns a list of concepts."
 Return the result as property list with following conversion rules:
 * Key => keyword
 * Array => list."
-  (request-response-data
-   (request (silver-brain-api--url url)
-            :parser (lambda ()
-                      (let* ((json-object-type 'plist)
-                             (json-key-type 'keyword)
-                             (json-array-type 'list))
-                        (json-read)))
-            :params params
-            :sync t)))
+  (http-response-data (http-request (silver-brain-api--url url))))
+
+(cl-defun silver-brain-api--request (url &key (method :get) data)
+  "Send request to URL with given DATA."
+  (http-request (silver-brain-api--url url) :method method :data data))
 
 (defun silver-brain-api--post (url &optional data)
   "Send POST request to the server with given URL and DATA.
 Returns the value of Location header."
-  (request-response-header
-   (request (silver-brain-api--url url)
-            :type "POST"
-            :data data
-            :sync t)
-   "Location"))
+  (http-response-location
+   (http-request (silver-brain-api--url url)
+                 :method :post
+                 :data data)))
 
 (defun silver-brain-api--put (url &optional data)
   "Send PUT request to the server with given URL and DATA."
-  (request (silver-brain-api--url url)
-           :type "PUT"
-           :data data
-           :sync t))
+  (http-request (silver-brain-api--url url)
+                :method :put
+                :data data))
 
 (defun silver-brain-api--delete (url)
   "Send DELETE request to the server with given URL."
-  (request (silver-brain-api--url url)
-           :type "DELETE"
-           :sync t))
+  (http-request (silver-brain-api--url url)
+                :method :delete))
+
+(cl-defun http-request (url &key (method :get) (data nil))
+  "Send HTTP request to URL with given METHOD and DATA."
+  (let ((url-request-method (case method
+                              (:get "GET")
+                              (:post "POST")
+                              (:put "PUT")
+                              (:delete "DELETE")))
+        (url-request-data data))
+    (with-current-buffer (url-retrieve-synchronously url)
+      (buffer-string))))
+
+(defun http-response-data (response)
+  "Extract HTTP body from RESPONSE and decode it."
+  (let* ((json-object-type 'plist)
+         (json-key-type 'keyword)
+         (json-array-type 'list))
+    (with-temp-buffer
+      (insert (http-response-body response))
+      (goto-char (point-min))
+      (json-read))))
+
+(defun http-response-body (response)
+  "Extract HTTP body from response."
+  (cl-subseq response (+ 2 (cl-search "\n\n" response))))
+
+(defun http-response-location (response)
+  "Extract HTTP Location header from response."
+  (let* ((start (cl-search "Location: " response))
+         (middle (+ 2 (cl-search ": " response :start2 start)))
+         (end (cl-search "\n" response :start2 start))
+         (match (cl-subseq response start end)))
+    (cl-subseq response middle end)))
 
 (defun silver-brain-api--url (uri)
   "Convert given resource URI to full URL."
