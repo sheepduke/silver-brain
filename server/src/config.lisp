2f7ce62ae982bcb31d5cf149a8df52a99f980a0f
YUE Daian <sheepduke@gmail.com>
2021-10-01 22:03:11 +0800

    feat: add config

---
+++ server/src/config.lisp
@@ -0,0 +1,12 @@
+(in-package silver-brain.config)
+
+(chameleon:defconfig
+  ;; Web server port.
+  (server-port 5000
+               "Server port.")
+  ;; Database file.
+  (database-file))
+
+(chameleon:defprofile :dev
+  (server-port 5001)
+  (database-file (truename "~/temp/a.sqlite")))
