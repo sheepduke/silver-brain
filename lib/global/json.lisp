45b30a2e30d4f884912c707be71ceb559b295c31
YUE Daian <sheepduke@gmail.com>
2023-04-13 01:32:13 +0800

    chore: delete Common Lisp code

--- lib/global/json.lisp
+++
@@ -1,9 +0,0 @@
-(in-package #:silver-brain.global)
-
-
-
-(with-auto-export ()
-  (defun setup ()
-    (setf shasht:*symbol-name-function*
-          #'slot-name->json-key)
-    (unlisp.dev:setup)))
--- lib/store/package.lisp
+++
@@ -1,7 +0,0 @@
-(unlisp:defpackage #:silver-brain.store.util
-  (:use #:unlisp))
-
-(in-package #:silver-brain.store.util)
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (unlisp.dev:setup-package-local-nicknames))
