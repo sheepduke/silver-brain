ee54dd55b2ea59b89b7fea3cf0678fe4328e8391
YUE Daian <sheepduke@gmail.com>
2023-04-16 23:27:52 +0800

    feat(server): setup skeleton for arg parser

--- server/src/Store/TestData.fs
+++ server/src/Store/TestData.fs
@@ -1,22 +1,13 @@
 namespace SilverBrain.Store
 
 open System.IO
+open System.Reflection
+open System.Text
 open RepoDb
 open Microsoft.Data.Sqlite
 
 module TestData =
-    // TODO: Support initializing test data from embedded resource.
-    let setup projectRoot rootDataFolder =
-        let scriptsRoot =
-            Path.Combine([| projectRoot; "server"; "src"; "Store"; "Scripts" |])
-
-        let migrationSqlRoot = Path.Combine(scriptsRoot, "Migration")
-        let testDataRoot = Path.Combine(scriptsRoot, "TestData")
-
-        // Get SQL content.
-        let dataSqlPath = Path.Combine(testDataRoot, "InitData.sql")
-        let dataSql = File.ReadAllText(dataSqlPath)
-
+    let private internalSetup migrationSqlLoadPolicy initDataSql rootDataFolder =
         // Create data directory.
         Directory.CreateDirectory(rootDataFolder) |> ignore
         Directory.CreateDirectory(Path.Combine(rootDataFolder, "attachments")) |> ignore
@@ -25,5 +16,32 @@ module TestData =
         let databasePath = Path.Combine(rootDataFolder, "silver-brain.sqlite")
         let conn = new SqliteConnection($"Data Source={databasePath}")
 
-        Migration.run (Migration.LoadSqlFromLocalDirectory migrationSqlRoot) true [ Path.Combine(databasePath) ]
-        conn.ExecuteNonQuery dataSql |> ignore
+        Migration.run migrationSqlLoadPolicy true [ Path.Combine(databasePath) ]
+        conn.ExecuteNonQuery initDataSql |> ignore
+
+    let setupFromEmbeddedResource rootDataFolder =
+        let currentAssembly = Assembly.GetExecutingAssembly()
+
+        use initDataSqlStream =
+            currentAssembly.GetManifestResourceStream("SilverBrain.src.Store.Scripts.TestData.InitData.sql")
+
+        use memoryStream = new MemoryStream()
+        initDataSqlStream.CopyTo(memoryStream)
+        let initDataSql = Encoding.UTF8.GetString(memoryStream.ToArray())
+
+        // let initDataSql =
+        internalSetup Migration.LoadSqlFromEmbeddedResource initDataSql rootDataFolder
+
+
+    let setupFromLocalFile projectRoot rootDataFolder =
+        let scriptsRoot =
+            Path.Combine([| projectRoot; "server"; "src"; "Store"; "Scripts" |])
+
+        let migrationSqlRoot = Path.Combine(scriptsRoot, "Migration")
+        let testDataRoot = Path.Combine(scriptsRoot, "TestData")
+
+        // Get SQL content.
+        let dataSqlPath = Path.Combine(testDataRoot, "InitData.sql")
+        let initDataSql = File.ReadAllText(dataSqlPath)
+
+        internalSetup (Migration.LoadSqlFromLocalDirectory migrationSqlRoot) initDataSql rootDataFolder
