edfc21028f9eabb6bfe84b67dc663d0ffeabe844
YUE Daian <sheepduke@gmail.com>
2023-05-02 00:01:09 +0800

    feat(server): change database schema about attachment

--- server/src/Store/Scripts/Migration/02.00.00_InitializeV2Schema.sql
+++ server/src/Store/Scripts/Migration/02.00.00_InitializeV2Schema.sql
@@ -1,38 +1,45 @@
-CREATE TABLE IF NOT EXISTS [Concept] (
-    [Uuid] TEXT PRIMARY KEY,
-    [Name] TEXT NOT NULL,
-    [CreatedAt] TEXT NOT NULL,
-    [UpdatedAt] TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS Concept (
+    Uuid TEXT PRIMARY KEY,
+    Name TEXT NOT NULL,
+    CreatedAt TEXT NOT NULL,
+    UpdatedAt TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS [ConceptAlias] (
-    [Id] INTEGER PRIMARY KEY,
-    [ConceptUuid] TEXT NOT NULL,
-    [Alias] TEXT NOT NULL,
-    FOREIGN KEY (ConceptUuid) REFERENCES [Concept] (Uuid)
+CREATE TABLE IF NOT EXISTS ConceptAlias (
+    Id INTEGER PRIMARY KEY,
+    ConceptUuid TEXT NOT NULL,
+    Alias TEXT NOT NULL,
+    FOREIGN KEY (ConceptUuid) REFERENCES Concept (Uuid)
 );
 
-CREATE TABLE if not exists [ConceptAttachment] (
-    [Id] INTEGER PRIMARY KEY,
-    [ConceptUuid] TEXT NOT NULL,
-    [Name] TEXT NOT NULL,
-    [ContentType] TEXT NOT NULL,
-    [ContentLength] INTEGER NOT NULL,
-    FOREIGN KEY (ConceptUuid) REFERENCES [Concept] (Uuid)
+CREATE TABLE if NOT EXISTS Attachment (
+    Id INTEGER PRIMARY KEY,
+    Name TEXT NOT NULL,
+    ContentType TEXT NOT NULL,
+    ContentLength INTEGER NOT NULL,
+    FilePath TEXT NOT NULL
 );
- 
-CREATE TABLE IF NOT EXISTS [ConceptRelationPair] (
-    [Id] INTEGER PRIMARY KEY,
-    [ConceptUuid] TEXT NOT NULL,
-    [OtherUuid] TEXT NOT NULL,
+
+CREATE TABLE if NOT EXISTS ConceptAttachment (
+    AttachmentId INTEGER NOT NULL,
+    ConceptUuid TEXT NOT NULL,
+    PRIMARY KEY (AttachmentId, ConceptUuid),
+    FOREIGN KEY (AttachmentId) REFERENCES Attachment (Id),
+    FOREIGN KEY (ConceptUuid) REFERENCES Concept (Uuid)
+);
+
+CREATE TABLE IF NOT EXISTS ConceptRelationPair (
+    Id INTEGER PRIMARY KEY,
+    ConceptUuid TEXT NOT NULL,
+    OtherUuid TEXT NOT NULL,
     UNIQUE (ConceptUuid, OtherUuid),
-    FOREIGN KEY (ConceptUuid) REFERENCES [Concept] (Uuid),
-    FOREIGN KEY (OtherUuid) REFERENCES [Concept] (Uuid)
+    FOREIGN KEY (ConceptUuid) REFERENCES Concept (Uuid),
+    FOREIGN KEY (OtherUuid) REFERENCES Concept (Uuid)
 );
 
-CREATE TABLE IF NOT EXISTS [ConceptLink] (
-    [Id] INTEGER PRIMARY KEY,
-    [SourceUuid] TEXT NOT NULL,
-    [RelationUuid] TEXT NOT NULL,
-    [TargetUuid] TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS ConceptLink (
+    Id INTEGER PRIMARY KEY,
+    SourceUuid TEXT NOT NULL,
+    RelationUuid TEXT NOT NULL,
+    TargetUuid TEXT NOT NULL
 );
