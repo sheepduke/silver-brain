8c30c9421f8fc7fa9bb469f032607c07654f1c1a
YUE Daian <sheepduke@gmail.com>
2023-05-13 01:09:34 +0800

    feat(server/dev): adjust fsi

--- src/server/tools/fsi.fs
+++ src/server/tools/fsi.fs
@@ -2,29 +2,22 @@
 #r "nuget:FSharpPlus"
 #r "nuget:Dapper.FSharp"
 #r "nuget:Microsoft.Data.Sqlite"
-#r "nuget:DbUp"
+#r "nuget:DbUp-Core"
 #r "nuget:DbUp-Sqlite"
 #r "nuget:FSharp.SystemTextJson"
 #r "nuget:Giraffe"
-#r "nuget:Microsoft.AspNetCore"
-#r "nuget:Microsoft.AspNetCore.Mvc"
-#r "nuget:Microsoft.Extensions.DependencyInjection"
-
 
 open FSharpPlus
 open System.IO
 open System.Text.Json
 open System.Text.Json.Serialization
 
-#load "../src/Core.fs"
-#load "../src/Store/Store.fs"
-#load "../src/Store/Migration.fs"
-#load "../src/Store/TestData.fs"
-#load "../src/Domain/CoreType.fs"
-#load "../src/Domain/ConceptMap/Type.fs"
-#load "../src/Domain/ConceptMap/ConceptMap.fs"
-#load "../src/RestApi/RestApi.fs"
-#load "../src/Cli/Dev.fs"
-#load "../src/Cli/Server.fs"
+#load "../dev/Core.fs"
+#load "../dev/DomainType/ConceptMap.fs"
+#load "../dev/Store/Store.fs"
+#load "../dev/Store/Migration.fs"
+#load "../dev/Store/TestData.fs"
+#load "../dev/Store/ConceptMapRepo.fs"
+#load "../dev/Domain/ConceptMap.fs"
 
 Dapper.FSharp.SQLite.OptionTypes.register ()
