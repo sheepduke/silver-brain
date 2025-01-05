8feb516ad59ad7db4a5312cb86a740fe21db6316
YUE Daian <sheepduke@gmail.com>
2025-01-05 21:40:34 +0800

    chore(scala): enhance data migration tool

--- tools/playground/src/main/scala/migrate.scala
+++ tools/playground/src/main/scala/migrate.scala
@@ -1,6 +1,5 @@
-//package silverbrain.tools.playground
+package silverbrain.tools.playground
 
-import silverbrain.core.*
 import silverbrain.store.DataRootPath
 import silverbrain.store.SqliteStoreManager
 
@@ -11,6 +10,9 @@ import java.time.Instant
 import org.flywaydb.core.Flyway
 import os.Path
 import scalikejdbc.*
+import silverbrain.store.SqlItemStore
+import silverbrain.store.Transactor
+import silverbrain.core.StoreName
 
 given JsonValueCodec[Map[String, String]] = JsonCodecMaker.make
 
@@ -38,8 +40,8 @@ case class ItemReference(
 
 type ItemLinkRow = (String, String, String)
 
-class OldStore(using DBSession):
-  def getItems(): Seq[Item] =
+class OldStore(transactor: Transactor):
+  def getItems()(using DBSession): Seq[Item] =
     sql"select id, props from item"
       .map(rs =>
         val id = "i_" + rs.string("id")
@@ -57,7 +59,7 @@ class OldStore(using DBSession):
       .list
       .apply()
 
-  def getItemLinks(): Seq[ItemLink] =
+  def getItemLinks()(using DBSession): Seq[ItemLink] =
     sql"SELECT * from item_child where parent in (select id from item) and child in (select id from item)"
       .map(rs =>
         ItemLink(
@@ -69,7 +71,7 @@ class OldStore(using DBSession):
       .list
       .apply()
 
-  def getItemReferences(): Seq[ItemReference] =
+  def getItemReferences()(using DBSession): Seq[ItemReference] =
     sql"select * from item_reference where source in (select id from item) and target in (select id from item)"
       .map(rs =>
         ItemReference(
@@ -84,57 +86,63 @@ class OldStore(using DBSession):
       .list
       .apply()
 
-class NewStore(using DBSession):
-  def insertItems(items: Seq[Item]): Unit =
+class NewStore(dbSession: DBSession):
+  given DBSession = dbSession
+
+  def insertItems(items: Seq[Item])(using DBSession): Unit =
     val params = items.map(item => item.productIterator.toSeq)
     sql"insert into item values (?, ?, ?, ?, ?, ?)".batch(params: _*).apply()
 
-  def insertItemLinks(links: Seq[ItemLink]): Unit =
+  def insertItemLinks(links: Seq[ItemLink])(using DBSession): Unit =
     val params = links.map(_.productIterator.toSeq)
     sql"insert into item_link values (?, ?, ?)".batch(params: _*).apply()
 
-  def insertItemReferences(references: Seq[ItemReference]): Unit =
+  def insertReferences(references: Seq[ItemReference])(using DBSession): Unit =
     val params = references.map(_.productIterator.toSeq)
     sql"insert into item_reference values (?, ?, ?, ?, ?, ?)"
       .batch(params: _*)
       .apply()
 
-def run() =
-  given DataRootPath = os.home / "temp" / "test"
+def getOldData(
+    transactor: Transactor,
+    storeName: StoreName
+): (Seq[Item], Seq[ItemLink], Seq[ItemReference]) =
+  val result = transactor.withTransaction(implicit session =>
+    val oldStore = OldStore(transactor)
+    val items = oldStore.getItems()
+    val links = oldStore.getItemLinks()
+    val references = oldStore.getItemReferences()
 
-  // def migrate(oldXa: Transactor[IO], newXa: Transactor[IO]): IO[Unit] = {
-  //   for
-  //     items <- getItems().transact(oldXa)
-  //     count <- insertItems(items).transact(newXa)
-  //     _ <- IO.println(s"Inserted ${count} items")
+    Right((items, links, references))
+  )(using storeName)
 
-  //     links <- getItemLinks().transact(oldXa)
-  //     count <- insertItemLinks(links).transact(newXa)
-  //     _ <- IO.println(s"Inserted ${count} links")
+  result.right.get
 
-  //     references <- getItemReferences().transact(oldXa)
-  //     count <- insertItemReferences(references).transact(newXa)
-  //     _ <- IO.println(s"Inserted ${count} references")
-  //   yield IO.pure(())
-  // }
+def insertNewData(
+    transactor: Transactor,
+    storeName: StoreName,
+    items: Seq[Item],
+    links: Seq[ItemLink],
+    references: Seq[ItemReference]
+): Unit =
+  transactor.withTransaction(implicit session =>
+    val newStore = NewStore(session)
 
-  // def createTransactor(path: Path) =
-  //   Transactor.fromDriverManager[IO](
-  //     driver = "org.sqlite.JDBC",
-  //     url = s"jdbc:sqlite:${path}",
-  //     logHandler = None
-  //   )
+    newStore.insertItems(items)
+    newStore.insertItemLinks(links)
+    newStore.insertReferences(references)
 
-  // lazy val oldXa = createTransactor(oldDatabasePath)
+    Right(())
+  )(using storeName)
 
-  // lazy val newXa = createTransactor(newDatabasePath)
+def migrate() =
+  val storeManager = SqliteStoreManager(os.home / "temp" / "test")
+  storeManager.create("main")
 
-  // def run() =
-  //   sql"delete from item".update.run.transact(newXa).unsafeRunSync()
-  //   sql"delete from item_link".update.run.transact(newXa).unsafeRunSync()
-  //   sql"delete from item_reference".update.run.transact(newXa).unsafeRunSync()
-  //   migrate(oldXa, newXa).unsafeRunSync()
+  val transactor = Transactor(storeManager)
 
-  // val oldDatabasePath = Path.expandUser("~/temp/test/old/data.sqlite")
+  val (items, links, references) = getOldData(transactor, "old")
 
-  // val newDatabasePath = Path.expandUser("~/temp/test/new/data.sqlite")
+  insertNewData(transactor, "main", items, links, references)
+
+  "Done"
