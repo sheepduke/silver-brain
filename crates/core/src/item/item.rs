e67161415296a7bbcecc86201b33e130a398a011
YUE Daian <sheepduke@gmail.com>
2025-05-27 11:36:06 +0800

    refactor(core): remove unused definition from Item

--- crates/core/src/item/item.rs
+++ crates/core/src/item/item.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use time::OffsetDateTime;
 use typed_builder::TypedBuilder;
 
-use super::{CoreItem, ItemId, ItemProperty, ItemReference};
+use super::{CoreItem, ItemId, ItemProperty};
 
 #[derive(Clone, Debug, TypedBuilder, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -31,10 +31,6 @@ pub struct Item {
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub children: Option<Vec<CoreItem>>,
-
-    #[builder(default, setter(strip_option))]
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub references: Option<Vec<ItemReference>>,
 
     #[builder(default, setter(strip_option))]
     #[serde(with = "time::serde::rfc3339::option")]
